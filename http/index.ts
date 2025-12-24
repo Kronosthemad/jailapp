7491e01d3a4049e21da1ab1bee76cc42393c7935
Eli Weatherby <chemicalorca22@gmail.com>
2025-12-24 06:58:01 -0600

    working on backend

---
+++ http/index.ts
@@ -0,0 +1,7 @@
+// index.ts
+// Query your database using the Prisma Client
+
+import { PrismaClient } from '@prisma/client'
+import { withAccelerate } from '@prisma/extension-accelerate'
+
+const prisma = new PrismaClient().$extends(withAccelerate())
