d0c8e0bae833338da50766aba0b04d388fe3990b
Eli Weatherby <chemicalorca22@gmail.com>
2025-12-25 19:23:30 -0600

    i have moved some stuff around still struggling with the backend im starting to think about adding python for apis i am not trying to mix so many languages so idk

--- http/index.js
+++ http/index.js
@@ -1,9 +1,6 @@
-document.getElementById('tester')?.addEventListener('click', function() {
-    alert('This Is Just A Test!');
-});
 
 function displayDate() {
     const el = document.getElementById('time');
     if (el) el.innerHTML = String(new Date());
 }
-setInterval(displayDate, 1000);
+ setInterval(displayDate, 1000);
--- http/src/server.js
+++ http/src/server.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = 5500;
 
 // Middleware
 app.use(express.json());
